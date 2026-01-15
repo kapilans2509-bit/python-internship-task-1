edb59ad5673dbb4c26e2b492bb2b40a9868a2a9e
kapilans2509-bit <kapilans.2509@gmail.com>
2026-01-15 23:05:37 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+# This program prints a greeting message
+# and takes user input for name and age
+
+# Print a welcome message
+print("Hello, welcome to my first Python program!")
+
+# Take user's name as input
+name = input("Enter your name: ")
+
+# Take user's age as input
+age = input("Enter your age: ")
+
+# Print the output using variables
+print("Nice to meet you,", name)
+print("You are", age, "years old.")
